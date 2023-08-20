edae630e1808050a69e1ca2841fc734ecafb1215
RobertHernandez21 <85580296+RobertHernandez21@users.noreply.github.com>
2023-08-20 09:47:38 -0500

    basic formatting

--- frontend/src/components/roster.vue
+++ frontend/src/components/roster.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div v-for="item in data" :key="item._id" class="col-md-4">
-        <div class="thumbnail">
-          <div class="caption">
-            <h3>{{ item.Name }}</h3>
-            <h3>{{ item.Position }}</h3>
-            <h3>{{ item.Number }}</h3>
-            <img :src="getImageUrl(item.image1)" class="img-thumbnail">
-            <br>
-          </div>
-        </div>
+   <div class="container mx-auto mt-8 py-8">
+    <h1 class="text-3xl text-red-500 mb-12 text-center">UH Men's Soccer Club Roster</h1>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-1">
+      <div v-for="item in data" :key="item._id" class="col-md-3">
+  <div class="thumbnail">
+    <div class="caption p-4 flex flex-col">
+      <img :src="getImageUrl(item.image1)" class=" object-cover w-52 h-64 ml-10">
+      <div class="mt-2 ml-10"> <!-- Add margin-left for shifting to the right -->
+        <h3 class="text-lg">{{ item.Name }}</h3>
+        <h3 class="text-md">{{ item.Position }}</h3>
+        <h3 class="text-md">{{ item.Number }}</h3>
       </div>
+    </div>
+  </div>
+</div>
+
     </div>
   </div>
 </template>
@@ -20,8 +23,8 @@
 import axios from 'axios';
 const apiURL = import.meta.env.VITE_ROOT_API;
 export default {
-name:'roster',
-data() {
+  name: 'roster',
+  data() {
     return {
       data: [],
     };
@@ -43,13 +46,8 @@ data() {
     this.fetchData();
   },
 };
-
-
 </script>
 
 <style scoped>
-img {
-  width: 100px;
-  height: 100px;
-}
+/* You can optionally add some scoped styles here */
 </style>
