cbb343aeedc2842ed3a1864dbb6a79c5e900cec1
RobertHernandez21 <85580296+RobertHernandez21@users.noreply.github.com>
2023-12-05 14:00:59 -0600

    j

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,24 @@
 <script>
 import Navbar from "./components/Navbar.vue"
+import Footer from "./components/footer.vue"
 export default {
   name:'App',
   components:{
     Navbar,
+    Footer,
   },
 }
 </script>
 
 <template>
   <div class="app">
+    
   
     <Navbar />
   
 
   <router-view></router-view>
-  
+  <Footer />
 </div>
 
 </template>
